fcd64fed1808a6db508c31241589410811d5bc89
Angela Edwards <edwards11914@outlook.com>
2019-03-19 22:46:52 -0400

    added js function for random quote

--- js/project.js
+++ js/project.js
@@ -12,6 +12,19 @@ $('a[href*="#"]').click(function(event){
 });
 
 
+var quotes =[
+ 'You can keep going and your legs might hurt for a week or you can quit and your mind will hurt for a lifetime-Mark Allen',
+ 'Triathlete a person who does not understand that one sport is enough',
+ 'Swim like the boat sunk ride like you stole it run for your life',
+ 'Triathlon does not build character it reveals it',
+ 'Love peace faith TRI do your best and forget the rest'
+]
+
+function newQuote() {
+var randomNumber = Math.floor(Math.random() * (quotes.length));
+document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+}
+
 
 
 
